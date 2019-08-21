299c70491e889a12da614ca3ecdb794694efc78f
farwayer <farwayer@gmail.com>
2019-08-21 03:18:01 +0300

    -;

--- src/utils.js
+++ src/utils.js
@@ -1,29 +1,29 @@
 export function setPrototypeOf(target, proto) {
   if (Object.setPrototypeOf) {
-    return Object.setPrototypeOf(target, proto);
+    return Object.setPrototypeOf(target, proto)
   }
 
-  target.__proto__ = proto;
-  return target;
+  target.__proto__ = proto
+  return target
 }
 
 export function getOwnPropertyDescriptors(obj) {
   if (Object.getOwnPropertyDescriptors) {
-    return Object.getOwnPropertyDescriptors(obj);
+    return Object.getOwnPropertyDescriptors(obj)
   }
 
   return Object.getOwnPropertyNames(obj).reduce((descs, prop) => {
-    descs[prop] = Object.getOwnPropertyDescriptor(obj, prop);
-    return descs;
-  }, {});
+    descs[prop] = Object.getOwnPropertyDescriptor(obj, prop)
+    return descs
+  }, {})
 }
 
 export function identity(val) {
-  return val;
+  return val
 }
 
 export function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 export function hasKeys(obj) {
