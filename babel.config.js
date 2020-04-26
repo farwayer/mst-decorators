b21fba9d24f07faf5fec42b937617fc0485e01bf
farwayer <farwayer@gmail.com>
2020-04-26 19:30:45 +0300

    back @babel/plugin-transform-runtime for tests

--- babel.config.js
+++ babel.config.js
@@ -11,5 +11,6 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', {legacy: true}],
     ['@babel/plugin-proposal-class-properties', {loose: true}],
+    ['@babel/plugin-transform-runtime', {useESModules: esm}],
   ],
 }
